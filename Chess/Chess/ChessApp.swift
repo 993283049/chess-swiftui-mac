7619f8d2f6c7da6e1b1c56b09216f2be4136cfd6
FordTan <993283049@qq.com>
2023-03-23 15:26:25 +0800

    created SwiftUI project for chess macOS

---
+++ Chess/Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by FordTan on 2023/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
